f7d66da3470e414bd172cc2e4f75c8c1ab3457bd
admin <raulacero9@gmail.com>
2018-01-24 19:28:30 +0100

    Ejercicio 2 multiplicar numeros de una lista

---
+++ multiplicar_lista_numeros.py
@@ -0,0 +1,11 @@
+"""
+Multiplicar todos los números de una lista
+"""
+
+numeros = [3, 5, 3, 1, 2, 3, 8]
+resultado = 1
+
+for numero in numeros:
+    resultado = numero * resultado
+
+print(resultado)
