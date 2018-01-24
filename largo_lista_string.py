e5f51bd7503857ad471eeae20ec5b6a316c21433
admin <raulacero9@gmail.com>
2018-01-24 19:24:08 +0100

    Ejercicio 1 largo string lista

---
+++ largo_lista_string.py
@@ -0,0 +1,11 @@
+"""
+Contar el largo de cada elemento de una lista
+"""
+
+lista = ['ardilla', 'cerdo', 'cordero', 'vaca']
+largo_lista = []
+
+for elemento in lista:
+    largo_lista.append(len(elemento))
+
+print(largo_lista)
