d536c40c6c61794f52665d7790f48f508cd156bb
Raul <raulacero9@gmail.com>
2018-02-23 22:27:20 +0100

    cambio de nombre en las variables para no volverme LOCO

--- libreria_datetime/cinco_dias_antes.py
+++ libreria_datetime/cinco_dias_antes.py
@@ -3,10 +3,10 @@ Crea un programa que te diga la fecha de hace 5 días respecto a hoy, y el día 
 """
 import datetime
 
-dias_semana = {"0": "lunes","1": "martes","2": "miercoles", "3": "jueves","4": "viernes","5": "sabado","6": "domingo"}
+semana = {"0": "lunes","1": "martes","2": "miercoles", "3": "jueves","4": "viernes","5": "sabado","6": "domingo"}
 hoy = datetime.datetime.now()
 restar = datetime.timedelta(days=5)
 hace_cinco_dias = hoy - restar
-dia_semana = dias_semana[str(hace_cinco_dias.weekday())]
+dia_semana = semana[str(hace_cinco_dias.weekday())]
 
 print(hace_cinco_dias.date(),dia_semana)
