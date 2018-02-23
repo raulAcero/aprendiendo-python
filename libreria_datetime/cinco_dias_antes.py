cc8a1dba295a43a09e7b56ab2335f96aef4d5bea
Raul <raulacero9@gmail.com>
2018-02-23 21:19:12 +0100

    ejercicio 1 diccionarios

---
+++ libreria_datetime/cinco_dias_antes.py
@@ -0,0 +1,12 @@
+"""
+Crea un programa que te diga la fecha de hace 5 días respecto a hoy, y el día de la semana.
+"""
+import datetime
+
+dias_semana = {"0": "lunes","1": "martes","2": "miercoles", "3": "jueves","4": "viernes","5": "sabado","6": "domingo"}
+hoy = datetime.datetime.now()
+restar = datetime.timedelta(days=5)
+hace_cinco_dias = hoy - restar
+dia_semana = dias_semana[str(hace_cinco_dias.weekday())]
+
+print(hace_cinco_dias.date(),dia_semana)
