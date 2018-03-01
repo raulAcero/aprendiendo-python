2f3ac0ae65b2b4783ab66667009f647943d5dc30
Raul <raulacero9@gmail.com>
2018-03-01 21:31:26 +0100

    Programa que calcula la fecha de muerte v2. Añadida variable para la muerte y día de la semana en el que se muere

--- libreria_datetime/calcular_muerte.py
+++ libreria_datetime/calcular_muerte.py
@@ -8,6 +8,7 @@ VIDA_MEDIA = 80
 PENALIZACION_FUMADOR = 10
 PENALIZACION_BEBEDOR = 10
 PENALIZACION_DEPORTISTA = 10
+ALIMENTACION = random.randint(0,15)
 
 def preguntar_si_no(pregunta,actividad):
     respuesta = ""
@@ -30,6 +31,7 @@ fecha_nacimiento = datetime.datetime.strptime(nacimiento, "%d/%m/%Y")
 fumar = preguntar_si_no("¿Fumas?", PENALIZACION_FUMADOR) or 0
 beber = preguntar_si_no("¿Bebes?", PENALIZACION_BEBEDOR) or 0
 deportista = preguntar_si_no("¿Haces deporte?", -PENALIZACION_DEPORTISTA) or 0
+dieta = preguntar_si_no("¿Haces una dieta equilibrada?", -ALIMENTACION) or 0
 
 anos_perdidos = int(fumar + beber + deportista)
 
@@ -39,4 +41,7 @@ muerte = fecha_nacimiento + datetime.timedelta(days=esperanza_vida*365)
 fecha_muerte = muerte.strftime("%d de %B del %Y")
 dias_restantes = muerte - datetime.datetime.now()
 
-print("Vas a morir el día {}, te quedan {} días".format(fecha_muerte, dias_restantes.days))
+semana = {"0": "lunes","1": "martes","2": "miercoles", "3": "jueves","4": "viernes","5": "sabado","6": "domingo"}
+dia_semana = semana[str(muerte.weekday())]
+
+print("Vas a morir el día {}, será un {}, te quedan {} días".format(fecha_muerte, dia_semana, dias_restantes.days))
